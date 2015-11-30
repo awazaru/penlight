d5bd83de3c318cdd3913f22b94e0a86d11f60320
awazaru <awazaru@yudaisMBA.local>
2015-11-30 20:42:31 +0900

    FirstCommit

---
+++ firmware/main.c
@@ -0,0 +1,16 @@
+/* Name: main.c
+ * Author: <insert your name here>
+ * Copyright: <insert your copyright message here>
+ * License: <insert your license reference here>
+ */
+
+#include <avr/io.h>
+
+int main(void)
+{
+
+    for(;;){
+
+    }
+    return 0;
+}
